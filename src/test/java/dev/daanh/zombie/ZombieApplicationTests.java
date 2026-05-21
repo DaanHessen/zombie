f345bfd7f493e08a0bfb38caa34e9f26135b79a5
Daan Hessen <daanh2002@gmail.com>
2026-05-21 14:47:43 +0200

    add world.puml and initialize SpringBoot

---
+++ src/test/java/dev/daanh/zombie/ZombieApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daanh.zombie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
